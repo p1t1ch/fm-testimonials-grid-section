fdb85a33ed7caf45a3dc39129b4abcbca206ea90
p1t1ch <belobeev.kirill@gmail.com>
2021-01-27 10:59:55 +0300

    setup theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,4 @@
 import 'focus-visible'
+import '@fontsource/barlow-semi-condensed/latin-500.css'
+import '@fontsource/barlow-semi-condensed/latin-600.css'
 import '@/styles/global.css'
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,25 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        DEFAULT: '#FFF',
+        purple: '#733FC8',
+        'grayish-blue': '#48556A',
+        'blackish-blue': '#19202D',
+      },
+      neutral: {
+        DEFAULT: '#CFCFCF',
+        blue: '#ECF2F8',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        normal: ['.8125rem', 1.38],
+      },
+    },
   },
 }
